b15976ee5226fbb734a19416ee1c30ef520f5f6d
petrux <giulio.petrucci@gmail.com>
2014-07-18 13:49:35 +0200

    changed host address to 0.0.0.0

--- ws23/app.py
+++ ws23/app.py
@@ -43,4 +43,6 @@ def home():
                            triples=triples)
 
 if __name__ == '__main__':
-    APP.run(debug=True, port=int(os.environ.get("PORT", "5000")))
+    APP.run(debug=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", "5000")))
